0a5e3bc7e91f3135f6fba602ea8bc7496980a792
Slaiiz <crocoshack@gmail.com>
2016-03-12 16:17:42 +0100

    Adapted error descriptions to fit with the new function names.

--- srcs/errors.c
+++ srcs/errors.c
@@ -3,14 +3,14 @@
 static void	init_errors(char **out)
 {
 	out[0]  = "Invalid/Missing argument(s)";
-	out[1]  = "Initialization failed";
-	out[2]  = "Sorting failed";
-	out[3]  = "Malloc failed";
-	out[4]  = "__push() failed";
-	out[5]  = "__rotate() failed";
-	out[6]  = "__reverse_rotate() failed";
+	out[1]  = "init_stacks() failed";
+	out[2]  = "sort_stacks() failed";
+	out[3]  = "malloc() failed";
+	out[4]  = "push() failed";
+	out[5]  = "rotate() failed";
+	out[6]  = "reverse_rotate() failed";
 	out[7]  = "Stack not large enough for action";
-	out[8]  = "__swap() failed";
+	out[8]  = "swap() failed";
 	out[9]  = "Invalid argument syntax";
 	out[10] = "No details";
 	out[11] = "Could not register operation";
@@ -35,12 +35,10 @@ void		print_error(int id, int arg)
 		if (level == 0)
 		{
 			ft_printf("{{red;b}}WE GOT AN ERROR CAPT'N!!!{{eoc;}}");
-			ft_printf("\nStack trace:\n* [%d] -> {{yellow}}%s{{eoc}} (%s)\n",
-				level++, errors[id], errors[arg]);
+			ft_printf("\nStack trace:\n*");
 		}
-		else
-			ft_printf("  [%d] -> {{yellow}}%s{{eoc}} (%s)\n",
-				level++, errors[id], errors[arg]);
+		ft_printf(" [%d] -> {{yellow}}%s{{eoc}} (%s)\n",
+			level++, errors[id], errors[arg]);
 	}
 	else if (level++ == 0)
 		ft_printf("#!fd=2^Error\n");
