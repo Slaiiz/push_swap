0a3b2f27343486adffc3b852d1a8dab5de3db41d
Slaiiz <crocoshack@gmail.com>
2016-03-20 12:17:40 +0100

    Norm fix.

--- libft/srcs/ft_arraycount.c
+++ libft/srcs/ft_arraycount.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_arraycount.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vchesnea <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/20 12:14:00 by vchesnea          #+#    #+#             */
+/*   Updated: 2016/03/20 12:14:02 by vchesnea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 int	ft_arraycount(void *a, int bytesize, int len, size_t n)
@@ -13,7 +25,7 @@ int	ft_arraycount(void *a, int bytesize, int len, size_t n)
 		else
 			break ;
 		a += bytesize * (i + 1);
-		len -=  i + 1;
+		len -= i + 1;
 	}
 	return (hits);
 }
