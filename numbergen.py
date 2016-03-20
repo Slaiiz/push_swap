dc581b2720e747d2051eff4d6906c3b3c412fda3
Slaiiz <crocoshack@gmail.com>
2016-03-20 12:16:13 +0100

    Added a number generator.

---
+++ numbergen.py
@@ -0,0 +1,16 @@
+from sys import stdout
+import random
+
+n = 15
+data = []
+random.seed();
+while n:
+	data.append(random.randint(-2000, 2000));
+	n -= 1
+i = iter(data)
+while True:
+	try:
+		stdout.write(str(i.next()))
+	except:
+		break
+	stdout.write(" ")
