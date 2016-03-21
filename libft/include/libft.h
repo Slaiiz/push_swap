068d73da9243fd553f812776ea27e74624f058c0
Slaiiz <crocoshack@gmail.com>
2016-03-21 18:20:34 +0100

    Added function ft_memdup() to libft.

--- libft/include/libft.h
+++ libft/include/libft.h
@@ -61,6 +61,7 @@ void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void				*ft_memmove(void *dst, const void *src, size_t len);
 void				*ft_memchr(const void *s, int c, size_t n);
 void				*ft_memalloc(size_t size);
+void				*ft_memdup(const void *m, int n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 int					ft_strcmp(const char *s1, const char *s2);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
