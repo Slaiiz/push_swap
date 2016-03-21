cdce2a59367c7d398a9bf564daf7a3866dd614af
Slaiiz <crocoshack@gmail.com>
2016-03-21 18:21:35 +0100

    Modified to work on longer ranges. Added shebang.

--- numbergen.py
+++ numbergen.py
@@ -1,14 +1,19 @@
+#!/usr/bin/python
+
 # HOW TO USE:
 # ./push_swap $(python numbergen.py)
+# OR: (under certain circumstances)
+# ./push_swap $(./numbergen.py)
+#
 
 from sys import stdout
 import random
 
-n = 15
+n = 100
 data = []
 random.seed();
 while n:
-	data.append(random.randint(-2000, 2000));
+	data.append(random.randint(-65536, 65535));
 	n -= 1
 i = iter(data)
 while True:
