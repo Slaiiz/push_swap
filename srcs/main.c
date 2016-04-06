6a3b35d953f563c15ac6d5e35b494924445f1ac1
Slaiiz <crocoshack@gmail.com>
2016-04-06 16:11:27 +0200

    Handy flag parsing.

--- srcs/main.c
+++ srcs/main.c
@@ -88,30 +88,59 @@ static int	sort_stacks(t_couple *c)
 	return (0);
 }
 
-static int	parse_flags(size_t *in, int *argc, char ***argv)
-{
-	char	*s;
+// static int	parse_flags(size_t *in, int *argc, char ***argv)
+// {
+// 	char	*s;
+//
+// 	while (*argc > 1 && (s = *(++*argv)) && s[0] == '-')
+// 	{
+// 		if (s[1] == 'v')
+// 			*in |= F_VERBOSE;
+// 		else if (s[1] == 'c')
+// 			*in |= F_COLOR;
+// 		else if (s[1] == 'g')
+// 			*in |= F_GAME | F_VERBOSE | F_STATS | F_ERRORS | F_COLOR;
+// 		else if (s[1] == 'e')
+// 			*in |= F_ERRORS;
+// 		else if (s[1] == 's')
+// 			*in |= F_STATS;
+// 		else if (ft_isdigit(s[1]))
+// 			return (0);
+// 		else if (*in & F_ERRORS)
+// 		{
+// 			print_error(INIT_ERRORS, (char*)F_ERRORS);
+// 			return (print_error(ERR_PARSEFAIL, "Illegal flag"));
+// 		}
+// 		--*argc;
+// 	}
+// 	return (0);
+// }
 
-	while (*argc > 1 && (s = *(++*argv)) && s[0] == '-')
+static int	parse_flags(int *argc, char ***argv, size_t *in)
+{
+	char	*args;
+
+	while (*argc && **(++*argv) == '-' && --*argc)
 	{
-		if (s[1] == 'v')
-			*in |= F_VERBOSE;
-		else if (s[1] == 'c')
-			*in |= F_COLOR;
-		else if (s[1] == 'g')
-			*in |= F_GAME | F_VERBOSE | F_STATS | F_ERRORS | F_COLOR;
-		else if (s[1] == 'e')
-			*in |= F_ERRORS;
-		else if (s[1] == 's')
-			*in |= F_STATS;
-		else if (ft_isdigit(s[1]))
-			return (0);
-		else if (*in & F_ERRORS)
+		args = **argv;
+		while (*++args != '\0')
 		{
-			print_error(INIT_ERRORS, (char*)F_ERRORS);
-			return (print_error(ERR_PARSEFAIL, "Illegal flag"));
+			if (*args == 'v')
+				*in |= F_VERBOSE;
+			else if (*args == 'c')
+				*in |= F_COLOR;
+			else if (*args == 'g')
+				*in |= F_GAME | F_VERBOSE | F_STATS | F_ERRORS | F_COLOR;
+			else if (*args == 'e')
+				*in |= F_ERRORS;
+			else if (*args == 's')
+				*in |= F_STATS;
+			else
+			{
+				print_error(INIT_ERRORS, (char*)F_ERRORS);
+				return (print_error(ERR_PARSEFAIL, "Illegal flag"));
+			}
 		}
-		--*argc;
 	}
 	return (0);
 }
@@ -121,7 +150,7 @@ int			main(int argc, char **argv)
 	t_couple	c;
 
 	ft_bzero(&c, sizeof(c));
-	if (parse_flags(&c.flags, &argc, &argv))
+	if (parse_flags(&argc, &argv, &c.flags))
 		return (print_error(ERR_MAINFAIL, "Undefined"));
 	print_error(INIT_ERRORS, (char*)c.flags);
 	if (argc < 2)
